074e5c36ececb2690fcaa31a1f800ed6a0fbc832
s00188563 <S00188563@mail.itsligo.ie>
2021-11-24 23:13:01 +0000

    landing page WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LandingComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
